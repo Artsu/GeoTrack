fbc258b8817f6f64c0d731f54cedf1f17d7a4d9d
Ari-Matti Nivasalo <arimatti.nivasalo@gmail.com>
2012-11-07 20:45:42 +0200

    -added base classes for needed activities

--- gen/com/example/geotrack/R.java
+++ gen/com/example/geotrack/R.java
@@ -30,15 +30,16 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int info_text=0x7f040003;
-        public static final int list_button_displaytext=0x7f040008;
-        public static final int map_button_displaytext=0x7f040009;
-        public static final int menu_settings=0x7f040004;
-        public static final int show_infotext=0x7f040002;
-        public static final int start_button_displaytext=0x7f040006;
-        public static final int stop_button_displaytext=0x7f040007;
-        public static final int title_activity_main=0x7f040005;
-        public static final int welcome_text=0x7f040001;
+        public static final int info_text=0x7f040004;
+        public static final int list_button_displaytext=0x7f040009;
+        public static final int map_activity_name=0x7f040001;
+        public static final int map_button_displaytext=0x7f04000a;
+        public static final int menu_settings=0x7f040005;
+        public static final int show_infotext=0x7f040003;
+        public static final int start_button_displaytext=0x7f040007;
+        public static final int stop_button_displaytext=0x7f040008;
+        public static final int title_activity_main=0x7f040006;
+        public static final int welcome_text=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
---
+++ src/fi/jyu/ties425/geotrack/MapActivity.java
@@ -0,0 +1,7 @@
+package fi.jyu.ties425.geotrack;
+
+import android.app.Activity;
+
+public class MapActivity extends Activity {
+
+}
