802c0087bb3be9ba3a7a1d5f4315cf75f5d86ee9
Ari-Matti Nivasalo <arimatti.nivasalo@gmail.com>
2012-11-12 18:24:46 +0200

    -added permissions for google maps\n-cleaned up the android manifest

--- src/fi/jyu/ties425/geotrack/PointsInMapActivity.java
+++ src/fi/jyu/ties425/geotrack/PointsInMapActivity.java
@@ -4,6 +4,7 @@ import com.example.geotrack.R;
 
 import android.os.Bundle;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
 public class PointsInMapActivity extends MapActivity {
@@ -15,6 +16,9 @@ public class PointsInMapActivity extends MapActivity {
         
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+//        MapController mapController = mapView.getController();
+//        mapController.setCenter(point)
     }
 
 	@Override
